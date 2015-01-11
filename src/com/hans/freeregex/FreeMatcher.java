6fd4a786303be11040bcf149b53bdc09308f816f
滕瀚斯 <hansjason@163.com>
2015-01-11 14:38:34 +0800

    基本完成核心主体

--- src/com/hans/freeregex/FreeMatcher.java
+++ src/com/hans/freeregex/FreeMatcher.java
@@ -1,5 +1,7 @@
 package com.hans.freeregex;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,40 +10,62 @@ public abstract class FreeMatcher {
 	public static final FreeMatcher EMAIL = new FreeMatcher() {
 		@Override
 		public Matcher getMatcher(CharSequence sequence) {
-			Pattern pattern = Pattern
-					.compile("[\\w[.-]]+@[\\w[.-]]+\\.[\\w]+");
-			return pattern.matcher(sequence);		
+			Pattern pattern = Pattern.compile("[\\w[.-]]+@[\\w[.-]]+\\.[\\w]+");
+			return pattern.matcher(sequence);
 		}
 	};
-	
+
 	public static FreeMatcher anyOf(String regex) {
 		return new FreeMatcher() {
 			@Override
 			public Matcher getMatcher(CharSequence sequence) {
-				Pattern pattern = Pattern
-						.compile(regex);
-				return pattern.matcher(sequence);		
+				Pattern pattern = Pattern.compile(regex);
+				return pattern.matcher(sequence);
 			}
 		};
-		
+
 	}
-	
+
 	abstract Matcher getMatcher(CharSequence sequence);
 
 	public boolean matchesAllOf(CharSequence sequence) {
 		return getMatcher(sequence).matches();
 	}
+
 	public boolean matchesAnyOf(CharSequence sequence) {
 		Matcher m = getMatcher(sequence);
 		m.reset();
 		return m.find();
 	}
 
-	public String retainFrom(CharSequence sequence) {
+	public String retainFirstFrom(CharSequence sequence) {
 		Matcher m = getMatcher(sequence);
-		if(m.find()) {
-			return  m.group();
+		if (m.find()) {
+			return m.group();
 		}
-		return null;	
+		return null;
+	}
+
+	public List<String> retainAllFrom(CharSequence sequence) {
+		Matcher m = getMatcher(sequence);
+		List<String> list = new ArrayList<>();
+		while (m.find()) {
+			list.add(m.group());
+		}
+		return list;
+	}
+
+	public String replaceFrom(CharSequence sequence, String replaceTo) {
+		return null;
+	}
+
+	public String replaceFrom(CharSequence sequence, FreeType replaceTo) {
+		Matcher m = getMatcher(sequence);
+		StringBuffer buf = new StringBuffer();
+		while (m.find()) {
+			m.appendReplacement(buf, replaceTo.replace(m.group()));
+		}
+		m.appendTail(buf);
+		return buf.toString();
 	}
 }
--- src/com/hans/test/Test.java
+++ src/com/hans/test/Test.java
@@ -1,42 +1,36 @@
 package com.hans.test;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import com.hans.freeregex.FreeMatcher;
-
-
+import com.hans.freeregex.FreeType;
 
 public class Test {
 	public static void main(String[] args) {
-/*		String a = "hans@124.com";
-		long s1 = System.currentTimeMillis();
-		for(int i=0;i<1000000;i++) {
-		a.matches("^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$");
-		}
-		long e1 = System.currentTimeMillis();
-		
-		long s2 = System.currentTimeMillis();
-		Pattern p = Pattern.compile("^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$");
-		Matcher m = p.matcher(a);
-		for(int i=0;i<1000000;i++) {
-			
-		m.matches();
-		}
-		long e2 = System.currentTimeMillis();
-		System.out.println(e1-s1);
-		System.out.println(e2-s2);*/
-		
-		String a = "asdf safsd@ASDF.de.co";
-		
-		
-		Pattern p = Pattern.compile("");
-		Matcher m = p.matcher(a);
-		System.out.println(m.find());
-				
-		System.out.println(FreeMatcher.EMAIL.matchesAllOf(a));
-		System.out.println(FreeMatcher.anyOf("").retainFrom(a));
-		//System.out.println(FreeMatcher.EMAIL.retainFrom(a));
+		String txtString = "ew10213asfas hasnf 10032 asdfa 12 1231254 ";
+		String regexString = "\\d+";
+		System.out.println(FreeMatcher.anyOf(regexString).replaceFrom(
+				txtString, new FreeType() {
+					public String replace(String A) {
+						
+						char[] charArray = A.toCharArray();
+						StringBuilder sb = new StringBuilder("");
+						int j = charArray.length;
+						for (int i = 0; j > 0;i++) {
+							if (i!=0&&i % 3 == 0) {
+								sb.insert(0, ',');
+							} 
+								sb.insert(0, charArray[j - 1]);
+								j--;
+								
+							
+						}
+						System.out.println(sb);
+						return sb.toString();
+					}
+				}));
+		/*
+		 * StringBuilder sb = new StringBuilder("adfasdf"); sb.insert(0, 'z');
+		 * System.out.println(sb);
+		 */
 
 	}
 }
